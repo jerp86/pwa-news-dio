ee57fbc62286279c818e4541ea6d81a800f05626
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-05-27 11:57:34 -0300

    add fetch api

---
+++ src/api.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
+const params = {
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+};
+
+const URL = "http://localhost:3333";
+
+const handleError = (err) =>
+  console.error("There was an error calling the API!!!", err);
+
+const getNews = (subject) =>
+  fetch(`${URL}/${subject}`, params)
+    .then((response) => response.json())
+    .catch(handleError);
+
+export default { getNews };
