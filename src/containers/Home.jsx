8d7d1c1e5cfa52c791e203fc7a18b022daaf3a3b
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-06-01 13:11:24 -0300

    fix structure and style

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -35,17 +35,14 @@ const Home = () => {
         <Col span={24} md={16}>
           <h2>World</h2>
         </Col>
-      </Row>
-      <hr />
-      <Row gutter={[16, 16]}>
-        <Col span={24} md={16}>
+        <Col span={24} md={8}>
           <h2>Economy</h2>
           <Economy values={news?.economy} />
         </Col>
       </Row>
       <hr />
       <Row gutter={[16, 16]}>
-        <Col span={24} md={16}>
+        <Col span={24}>
           <h2>Technology</h2>
         </Col>
       </Row>
