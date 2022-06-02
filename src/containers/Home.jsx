534b8808fe34810f45a75a3f4a20d3246b448386
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-06-02 16:26:53 -0300

    fix styles

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,12 @@
 import React, { memo, useEffect, useState } from 'react';
-import { Col, Row } from 'antd';
+import { Col, Row, Typography } from 'antd';
 
 import api from '../api';
 import Economy from './components/Economy';
 import Technology from './components/Technology';
 import World from './components/World';
+
+const { Title } = Typography;
 
 const Home = () => {
   const [news, setNews] = useState({});
@@ -35,18 +37,18 @@ const Home = () => {
     <div>
       <Row gutter={[16, 16]}>
         <Col span={24} md={16}>
-          <h2>World</h2>
+          <Title level={2}>World</Title>
           <World values={news?.world} />
         </Col>
         <Col span={24} md={8}>
-          <h2>Economy</h2>
+          <Title level={2}>Economy</Title>
           <Economy values={news?.economy} />
         </Col>
       </Row>
       <hr />
       <Row gutter={[16, 16]}>
         <Col span={24}>
-          <h2>Technology</h2>
+          <Title level={2}>Technology</Title>
           <Technology values={news?.technology} />
         </Col>
       </Row>
